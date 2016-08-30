a930063a01e5d71134fd1ab982584e275ebd72d5
elcirao <alejandrosoto@out-330016.local>
2016-08-30 17:23:28 -0400

    avance

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-// $(function(){
-//   //IF YOU CLICK ON AN ELEMETN a
-//   $(document).on('click', 'a', function(ev){
-//     ev.preventDefault(); //PREVENT DEFAULT ACTION FOR THE DOM
-//     id = $(this).attr('id');
-//     $.getJSON('test.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
-//       for (var i = 0; i < data.razas.length; i++) { //LOOK FOR ALL THE DATA ON THE JSON
-//         if(id == data.razas[i].id){ //IF THE OPTION SELECTED EQUAL TO ID FROM THE ELEMENT
-//           $('#desc').text(data.razas[i].Caracteristicas); //FILL THE H1 WITH Caracteristicas, IF YOU WANNA CHANGE THE DOM, YOU JUST CHANGE THE ID
-//           $('#img_lista').attr('src', data.razas[i].imagen) //FILL THE IMG WITH imagen
-//           $('#test_select').val(id);
-//         }
-//       }
-//     });
-//   });
-// });
+$(function(){
+  //IF YOU CLICK ON AN ELEMETN a
+  $(document).on('click', $('.items-producto'), function(ev){
+    ev.preventDefault(); //PREVENT DEFAULT ACTION FOR THE DOM
+    id = $(this).attr('id');
+    $.getJSON('src/productos.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
+      for (var i = 0; i < data.productos.length; i++) { //LOOK FOR ALL THE DATA ON THE JSON
+        if(id == data.productos[i].id){ //IF THE OPTION SELECTED EQUAL TO ID FROM THE ELEMENT
+          $('#desc').text(data.productos[i].Caracteristicas); //FILL THE H1 WITH Caracteristicas, IF YOU WANNA CHANGE THE DOM, YOU JUST CHANGE THE ID
+          $('#img_lista').attr('src', data.productos[i].imagen) //FILL THE IMG WITH imagen
+          $('#test_select').val(id);
+        }
+      }
+    });
+  });
+});
 // //IF YOU CHOOSE A DIFFERENT OPTION FROM THE SELECT
 // $(document).on('change', $('#test_select'), function(el){ //el IS THE ELEMENT, IN THIS CASE =  $('#test_select')
 //   $.getJSON('test.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
