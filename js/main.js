1ff009642d297b458715aa17e3d41037186209d0
elcirao <alejandrosoto@out-330016.local>
2016-08-31 12:19:36 -0400

    merge issue-3

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
-$(function(){
-  //IF YOU CLICK ON AN ELEMETN a
-  $(document).on('click', $('.items-producto'), function(ev){
-    ev.preventDefault(); //PREVENT DEFAULT ACTION FOR THE DOM
-    id = $(this).attr('id');
-    $.getJSON('src/productos.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
-      for (var i = 0; i < data.productos.length; i++) { //LOOK FOR ALL THE DATA ON THE JSON
-        if(id == data.productos[i].id){ //IF THE OPTION SELECTED EQUAL TO ID FROM THE ELEMENT
-          $('#desc').text(data.productos[i].Caracteristicas); //FILL THE H1 WITH Caracteristicas, IF YOU WANNA CHANGE THE DOM, YOU JUST CHANGE THE ID
-          $('#img_lista').attr('src', data.productos[i].imagen) //FILL THE IMG WITH imagen
-          $('#test_select').val(id);
-        }
+
+//IF YOU CLICK ON AN ELEMETN a
+$(document).on('click', '.items-producto', function(ev){
+  ev.preventDefault(); //PREVENT DEFAULT ACTION FOR THE DOM
+  id = $(this).attr('data-tipo-perro');
+  $.getJSON('src/productos.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
+    for (var i = 0; i < data.tipoPerro.length; i++) { //LOOK FOR ALL THE DATA ON THE JSON
+      if(id == data.tipoPerro[i].id){ //IF THE OPTION SELECTED EQUAL TO ID FROM THE ELEMENT
+        alert(data.tipoPerro[i].tipoProductos[0].productos[0].Nombre)
+        // $('#desc').text(data.productos[i].Caracteristicas); //FILL THE H1 WITH Caracteristicas, IF YOU WANNA CHANGE THE DOM, YOU JUST CHANGE THE ID
+        // $('#img_lista').attr('src', data.productos[i].imagen) //FILL THE IMG WITH imagen
+        // $('#test_select').val(id);
       }
-    });
+    }
   });
 });
+
+$(document).on('click', '.figure-pruducto', function(ev){
+  ev.preventDefault(); //PREVENT DEFAULT ACTION FOR THE DOM
+  id = $(this).attr('data-tipo-perro');
+  $.getJSON('src/productos.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
+    for (var i = 0; i < data.tipoPerro.length; i++) { //LOOK FOR ALL THE DATA ON THE JSON
+      if(id == data.tipoPerro[i].id){ //IF THE OPTION SELECTED EQUAL TO ID FROM THE ELEMENT
+        $('.container-fluid').css('display', 'block');
+        // $('#desc').text(data.productos[i].Caracteristicas); //FILL THE H1 WITH Caracteristicas, IF YOU WANNA CHANGE THE DOM, YOU JUST CHANGE THE ID
+        // $('#img_lista').attr('src', data.productos[i].imagen) //FILL THE IMG WITH imagen
+        // $('#test_select').val(id);
+      }
+    }
+  });
+});
+
 // //IF YOU CHOOSE A DIFFERENT OPTION FROM THE SELECT
 // $(document).on('change', $('#test_select'), function(el){ //el IS THE ELEMENT, IN THIS CASE =  $('#test_select')
 //   $.getJSON('test.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
@@ -26,18 +42,18 @@ $(function(){
 //   });
 // });
 //FILL DOMS WITH DATA FROM JSON
-$(function(){ //WHEN DOCUMENT IS READY
-  $.getJSON('src/productos.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
-    for (var i = 0; i < data.productos.length; i++) { //LOOK FOR ALL THE DATA ON THE JSON
-      if(i == 0){
-        $('#productos-ol').append('<li data-target="#myCarousel" data-slide-to='+i+' class="active"></li>')
-        $('#wrapper-productos').append('<div class="item active"><img src='+data.productos[i].imagen+' width="460" height="345"></div>')
-      }else{
-        $('#productos-ol').append('<li data-target="#myCarousel" data-slide-to='+i+'></li>')
-        $('#wrapper-productos').append('<div class="item"><img src='+data.productos[i].imagen+' width="460" height="345"></div>')
-      }
-      $('#lista').append('<li><a href="" class="link-img" id='+data.productos[i].id+'>'+data.productos[i].Nombre+'</a></li>');
-      //$('#test_select').append('<option value='+data.productos[i].id+'>'+data.productos[i].id+'</option>');
-    }
-  });
-});
+// $(function(){ //WHEN DOCUMENT IS READY
+//   $.getJSON('src/productos.json', function(data){ //GET THE JSON AND data IS THE DATA FROM THE JSON
+//     for (var i = 0; i < data.productos.length; i++) { //LOOK FOR ALL THE DATA ON THE JSON
+//       if(i == 0){
+//         $('#productos-ol').append('<li data-target="#myCarousel" data-slide-to='+i+' class="active"></li>')
+//         $('#wrapper-productos').append('<div class="item active"><img src='+data.productos[i].imagen+' width="460" height="345"></div>')
+//       }else{
+//         $('#productos-ol').append('<li data-target="#myCarousel" data-slide-to='+i+'></li>')
+//         $('#wrapper-productos').append('<div class="item"><img src='+data.productos[i].imagen+' width="460" height="345"></div>')
+//       }
+//       $('#lista').append('<li><a href="" class="link-img" id='+data.productos[i].id+'>'+data.productos[i].Nombre+'</a></li>');
+//       //$('#test_select').append('<option value='+data.productos[i].id+'>'+data.productos[i].id+'</option>');
+//     }
+//   });
+// });
