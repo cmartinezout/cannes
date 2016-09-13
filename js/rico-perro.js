6484504a9ee97dfd94f80c08f10e5a57f33cc20e
elcirao <alejandrosoto@out-330016.local>
2016-09-13 19:51:30 -0400

    add razas

---
+++ js/rico-perro.js
@@ -0,0 +1,33 @@
+$(document).on('click','.figure-rico' ,function(e){
+  e.preventDefault();
+  var id_tipo = parseInt($(this).attr('data-tipo-seccion'));
+  $('.section-rico-perro').css('display', 'none');
+  $("section[tipo='"+id_tipo+"']").css('display', 'block');
+  $('html, body').stop().animate({
+    'scrollTop': $("section[tipo='"+id_tipo+"']").offset().top-100
+  }, 900, 'swing', function () {
+  });
+  if(id_tipo == 1) {
+    $.getJSON('src/razas.json', function(){
+    }).done(function(data){
+      console.log(data);
+    });
+  }
+});
+
+$(document).on('click', '.perro' ,function(e){
+  e.preventDefault();
+  var id_tipo = parseInt($(this).attr('data-tipo-seccion'));
+  $('.section-rico-perro').css('display', 'none');
+  $("section[tipo='"+id_tipo+"']").css('display', 'block');
+  $('html, body').stop().animate({
+    'scrollTop': $("section[tipo='"+id_tipo+"']").offset().top-100
+  }, 900, 'swing', function () {
+  });
+  if(id_tipo == 1) {
+    $.getJSON('src/razas.json', function(){
+    }).done(function(data){
+      console.log(data);
+    });
+  }
+});
